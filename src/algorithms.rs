69536e72603f4dc671ec426ed2f89302e2ed54a2
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-08-14 19:31:10 +1000

    Slight changes to ReadDecode and algorithm changes. I'm going to restructure the algorithms for the last time in the next push

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -6,6 +6,6 @@ use crate::errors::decompression_error::DecompressionError;
 pub trait Algorithm {
     fn finalise_encode(&self, meta: &AlgorithmMeta) -> Result<&[u8], CompressionError>;
     fn finalise_decode(&self, meta: &AlgorithmMeta) -> Result<&[u8], DecompressionError>;
-    fn partial_encode(&self, data: &[u8], meta: &AlgorithmMeta) -> Result<&[u8], CompressionError>;
-    fn partial_decode(&self, data: &[u8], meta: &AlgorithmMeta) -> Result<&[u8], DecompressionError>;
+    fn partial_encode(&self, data: &[u8], meta: &AlgorithmMeta) -> Result<Vec<u8>, CompressionError>;
+    fn partial_decode(&self, data: &[u8], meta: &AlgorithmMeta) -> Result<Vec<u8>, DecompressionError>;
 }
--- src/read/decompress.rs
+++ src/read/decompress.rs
@@ -1,10 +1,9 @@
 use std::fs::File;
-use std::io;
-use std::io::{BufReader, Read};
+use std::io::Read;
+
 use crate::AlgorithmMeta;
 use crate::algorithms::Algorithm;
 use crate::errors::decompression_error::DecompressionError;
-use crate::write::decompress::WriteDecoder;
 
 pub struct ReadDecoder<'a, T: Algorithm> {
     meta: AlgorithmMeta,
@@ -23,33 +22,23 @@ impl<'a, T: Algorithm> ReadDecoder<'a, T> {
 
     /// Attempts to write a buffer and returns how many bytes were written to the writer.
     // TODO: Add in errors
-    pub fn read(&mut self, buffer: &mut [u8]) -> io::Result<usize> {
-        self.origin.read(buffer).unwrap();
-        self.encoder.partial_decode();
-        return Ok(0);
+    pub fn read<const S: usize>(&mut self) -> Result<Vec<u8>, DecompressionError> {
+        let mut buf: [u8; S] = [0; S];
+        self.origin.read_exact(&mut buf).unwrap();
+        return self.encoder.partial_decode(&buf, &self.meta);
     }
 
-    /// Attempts to write all provided data to the buffer
-    pub fn read_all(&mut self, data: &[u8]) -> io::Result<usize> {
-        self.buffer.extend(data);
-        return Ok(0);
-    }
+    /// Attempts to read and decompress all remaining data within the origin file
+    pub fn read_all(&mut self) -> Result<Vec<u8>, DecompressionError> {
+        let mut buf = Vec::new();
+        self.origin.read_to_end(&mut buf).unwrap();
 
-    /// Compresses all data within the buffer and writes it to disk
-    // TODO: Add in errors
-    pub fn flush(&mut self) -> io::Result<()> {
-        let data = self.encoder.partial_decode(self.buffer, &self.meta).unwrap();
-        self.destination.write_all(data).unwrap();
-        self.buffer.clear();
-        return Ok(())
+        return self.encoder.partial_decode(&*buf, &self.meta);
     }
 
     /// Finalises the data that is being written to disk and writes any tail data. Returns to File
     /// object where the compressed data is now stored
-    pub fn finish(mut self) -> io::Result<File> {
-        let enc = self.encoder.finalise_decode(&self.meta).unwrap();
-        self.write_all(enc).unwrap();
-        self.flush().unwrap();
-        Ok(self.destination)
+    pub fn finish(mut self) -> Result<Vec<u8>, DecompressionError> {
+        Ok(Vec::from(self.encoder.finalise_decode(&self.meta).unwrap()))
     }
 }
