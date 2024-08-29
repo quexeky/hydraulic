08863efaceede157252c8be48f47561bcdca870c
Louis van Liefland <116044207+quexeky@users.noreply.github.com>
2024-08-29 10:29:46 +1000

    Struggling against clap-stdin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 mod cli;
 
-use std::fs;
-use std::fs::File;
 use clap::Parser;
-use hydraulic::Gzip;
-
-use hydraulic::prelude::*;
 use crate::cli::args::Args;
 
 fn main() {
     let args = Args::parse();
+    println!("{:?}", args);
 }
